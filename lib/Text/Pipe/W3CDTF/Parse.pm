5990f1b8e8792cb7acd300c2ebd80d136f496432
Marcel Grünauer <marcel@cpan.org>
2007-12-07 15:40:57 +0000

    Import of MARCEL/Text-Pipe-W3CDTF-0.01 from CPAN.
    gitpan-cpan-distribution: Text-Pipe-W3CDTF
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         MARCEL/Text-Pipe-W3CDTF-0.01.tar.gz
    gitpan-cpan-author:       MARCEL
    gitpan-cpan-maturity:     released

---
+++ lib/Text/Pipe/W3CDTF/Parse.pm
@@ -0,0 +1,149 @@
+package Text::Pipe::W3CDTF::Parse;
+
+use strict;
+use warnings;
+use DateTime::Format::W3CDTF;
+
+
+our $VERSION = '0.01';
+
+
+use base qw(Text::Pipe::W3CDTF);
+
+
+sub filter_single {
+    my ($self, $input) = @_;
+    DateTime::Format::W3CDTF->parse_datetime($input);
+}
+
+
+1;
+
+
+__END__
+
+
+
+=head1 NAME
+
+Text::Pipe::W3CDTF::Parse - Parse a W3CDTF datetime string into a DateTime object
+
+=head1 SYNOPSIS
+
+    Text::Pipe::W3CDTF::Parse->new;
+
+=head1 DESCRIPTION
+
+=head1 METHODS
+
+=over 4
+
+
+
+=item filter_single
+
+Takes a single W3CDTF datetime string and parses it into a L<DateTime> object.
+If given an improperly formatted string, this method may die.
+
+=back
+
+Text::Pipe::W3CDTF::Parse inherits from L<Text::Pipe::W3CDTF>.
+
+The superclass L<Text::Pipe::Base> defines these methods and functions:
+
+    new(), bit_or(), filter(), init()
+
+The superclass L<Class::Accessor::Complex> defines these methods and
+functions:
+
+    mk_abstract_accessors(), mk_array_accessors(), mk_boolean_accessors(),
+    mk_class_array_accessors(), mk_class_hash_accessors(),
+    mk_class_scalar_accessors(), mk_concat_accessors(),
+    mk_forward_accessors(), mk_hash_accessors(), mk_integer_accessors(),
+    mk_new(), mk_object_accessors(), mk_scalar_accessors(),
+    mk_set_accessors(), mk_singleton()
+
+The superclass L<Class::Accessor> defines these methods and functions:
+
+    _carp(), _croak(), _mk_accessors(), accessor_name_for(),
+    best_practice_accessor_name_for(), best_practice_mutator_name_for(),
+    follow_best_practice(), get(), make_accessor(), make_ro_accessor(),
+    make_wo_accessor(), mk_accessors(), mk_ro_accessors(),
+    mk_wo_accessors(), mutator_name_for(), set()
+
+The superclass L<Class::Accessor::Installer> defines these methods and
+functions:
+
+    install_accessor()
+
+The superclass L<Class::Accessor::Constructor> defines these methods and
+functions:
+
+    _make_constructor(), mk_constructor(), mk_constructor_with_dirty(),
+    mk_singleton_constructor()
+
+The superclass L<Data::Inherited> defines these methods and functions:
+
+    every_hash(), every_list(), flush_every_cache_by_key()
+
+The superclass L<Class::Accessor::Constructor::Base> defines these methods
+and functions:
+
+    STORE(), clear_dirty(), clear_hygienic(), clear_unhygienic(),
+    contains_hygienic(), contains_unhygienic(), delete_hygienic(),
+    delete_unhygienic(), dirty(), dirty_clear(), dirty_set(),
+    elements_hygienic(), elements_unhygienic(), hygienic(),
+    hygienic_clear(), hygienic_contains(), hygienic_delete(),
+    hygienic_elements(), hygienic_insert(), hygienic_is_empty(),
+    hygienic_size(), insert_hygienic(), insert_unhygienic(),
+    is_empty_hygienic(), is_empty_unhygienic(), set_dirty(),
+    size_hygienic(), size_unhygienic(), unhygienic(), unhygienic_clear(),
+    unhygienic_contains(), unhygienic_delete(), unhygienic_elements(),
+    unhygienic_insert(), unhygienic_is_empty(), unhygienic_size()
+
+The superclass L<Tie::StdHash> defines these methods and functions:
+
+    CLEAR(), DELETE(), EXISTS(), FETCH(), FIRSTKEY(), NEXTKEY(), SCALAR(),
+    TIEHASH()
+
+=head1 TAGS
+
+If you talk about this module in blogs, on del.icio.us or anywhere else,
+please use the C<textpipew3cdtf> tag.
+
+=head1 VERSION 
+                   
+This document describes version 0.01 of L<Text::Pipe::W3CDTF::Parse>.
+
+=head1 BUGS AND LIMITATIONS
+
+No bugs have been reported.
+
+Please report any bugs or feature requests to
+C<<bug-text-pipe-w3cdtf@rt.cpan.org>>, or through the web interface at
+L<http://rt.cpan.org>.
+
+=head1 INSTALLATION
+
+See perlmodinstall for information and options on installing Perl modules.
+
+=head1 AVAILABILITY
+
+The latest version of this module is available from the Comprehensive Perl
+Archive Network (CPAN). Visit <http://www.perl.com/CPAN/> to find a CPAN
+site near you. Or see <http://www.perl.com/CPAN/authors/id/M/MA/MARCEL/>.
+
+=head1 AUTHOR
+
+Marcel GrE<uuml>nauer, C<< <marcel@cpan.org> >>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright 2007 by Marcel GrE<uuml>nauer
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+
+=cut
+
