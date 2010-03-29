1fc7257b99e488d76a768f389f9b0305fe81ff86
Marcel Grünauer <marcel@cpan.org>
2010-03-29 22:50:45 +0000

    Import of MARCEL/Text-Pipe-W3CDTF-1.100890 from CPAN.
    gitpan-cpan-distribution: Text-Pipe-W3CDTF
    gitpan-cpan-version:      1.100890
    gitpan-cpan-path:         MARCEL/Text-Pipe-W3CDTF-1.100890.tar.gz
    gitpan-cpan-author:       MARCEL
    gitpan-cpan-maturity:     released

--- lib/Text/Pipe/W3CDTF/Parse.pm
+++ lib/Text/Pipe/W3CDTF/Parse.pm
@@ -1,149 +1,73 @@
-package Text::Pipe::W3CDTF::Parse;
-
+use 5.008;
 use strict;
 use warnings;
+
+package Text::Pipe::W3CDTF::Parse;
+our $VERSION = '1.100890';
+# ABSTRACT: Parse a W3CDTF date-time string into a DateTime object
 use DateTime::Format::W3CDTF;
-
-
-our $VERSION = '0.01';
-
-
-use base qw(Text::Pipe::W3CDTF);
-
+use parent qw(Text::Pipe::W3CDTF);
 
 sub filter_single {
     my ($self, $input) = @_;
     DateTime::Format::W3CDTF->parse_datetime($input);
 }
-
-
 1;
 
 
 __END__
+=pod
 
-
+=for stopwords DateTime
 
 =head1 NAME
 
-Text::Pipe::W3CDTF::Parse - Parse a W3CDTF datetime string into a DateTime object
+Text::Pipe::W3CDTF::Parse - Parse a W3CDTF date-time string into a DateTime object
 
-=head1 SYNOPSIS
+=head1 VERSION
 
-    Text::Pipe::W3CDTF::Parse->new;
-
-=head1 DESCRIPTION
+version 1.100890
 
 =head1 METHODS
 
-=over 4
+=head2 filter_single
 
-
-
-=item filter_single
-
-Takes a single W3CDTF datetime string and parses it into a L<DateTime> object.
-If given an improperly formatted string, this method may die.
-
-=back
-
-Text::Pipe::W3CDTF::Parse inherits from L<Text::Pipe::W3CDTF>.
-
-The superclass L<Text::Pipe::Base> defines these methods and functions:
-
-    new(), bit_or(), filter(), init()
-
-The superclass L<Class::Accessor::Complex> defines these methods and
-functions:
-
-    mk_abstract_accessors(), mk_array_accessors(), mk_boolean_accessors(),
-    mk_class_array_accessors(), mk_class_hash_accessors(),
-    mk_class_scalar_accessors(), mk_concat_accessors(),
-    mk_forward_accessors(), mk_hash_accessors(), mk_integer_accessors(),
-    mk_new(), mk_object_accessors(), mk_scalar_accessors(),
-    mk_set_accessors(), mk_singleton()
-
-The superclass L<Class::Accessor> defines these methods and functions:
-
-    _carp(), _croak(), _mk_accessors(), accessor_name_for(),
-    best_practice_accessor_name_for(), best_practice_mutator_name_for(),
-    follow_best_practice(), get(), make_accessor(), make_ro_accessor(),
-    make_wo_accessor(), mk_accessors(), mk_ro_accessors(),
-    mk_wo_accessors(), mutator_name_for(), set()
-
-The superclass L<Class::Accessor::Installer> defines these methods and
-functions:
-
-    install_accessor()
-
-The superclass L<Class::Accessor::Constructor> defines these methods and
-functions:
-
-    _make_constructor(), mk_constructor(), mk_constructor_with_dirty(),
-    mk_singleton_constructor()
-
-The superclass L<Data::Inherited> defines these methods and functions:
-
-    every_hash(), every_list(), flush_every_cache_by_key()
-
-The superclass L<Class::Accessor::Constructor::Base> defines these methods
-and functions:
-
-    STORE(), clear_dirty(), clear_hygienic(), clear_unhygienic(),
-    contains_hygienic(), contains_unhygienic(), delete_hygienic(),
-    delete_unhygienic(), dirty(), dirty_clear(), dirty_set(),
-    elements_hygienic(), elements_unhygienic(), hygienic(),
-    hygienic_clear(), hygienic_contains(), hygienic_delete(),
-    hygienic_elements(), hygienic_insert(), hygienic_is_empty(),
-    hygienic_size(), insert_hygienic(), insert_unhygienic(),
-    is_empty_hygienic(), is_empty_unhygienic(), set_dirty(),
-    size_hygienic(), size_unhygienic(), unhygienic(), unhygienic_clear(),
-    unhygienic_contains(), unhygienic_delete(), unhygienic_elements(),
-    unhygienic_insert(), unhygienic_is_empty(), unhygienic_size()
-
-The superclass L<Tie::StdHash> defines these methods and functions:
-
-    CLEAR(), DELETE(), EXISTS(), FETCH(), FIRSTKEY(), NEXTKEY(), SCALAR(),
-    TIEHASH()
-
-=head1 TAGS
-
-If you talk about this module in blogs, on del.icio.us or anywhere else,
-please use the C<textpipew3cdtf> tag.
-
-=head1 VERSION 
-                   
-This document describes version 0.01 of L<Text::Pipe::W3CDTF::Parse>.
-
-=head1 BUGS AND LIMITATIONS
-
-No bugs have been reported.
-
-Please report any bugs or feature requests to
-C<<bug-text-pipe-w3cdtf@rt.cpan.org>>, or through the web interface at
-L<http://rt.cpan.org>.
+Takes a single W3CDTF date-time string and parses it into a L<DateTime>
+object. If given an improperly formatted string, this method may die.
 
 =head1 INSTALLATION
 
 See perlmodinstall for information and options on installing Perl modules.
 
+=head1 BUGS AND LIMITATIONS
+
+No bugs have been reported.
+
+Please report any bugs or feature requests through the web interface at
+L<http://rt.cpan.org/Public/Dist/Display.html?Name=Text-Pipe-W3CDTF>.
+
 =head1 AVAILABILITY
 
 The latest version of this module is available from the Comprehensive Perl
-Archive Network (CPAN). Visit <http://www.perl.com/CPAN/> to find a CPAN
-site near you. Or see <http://www.perl.com/CPAN/authors/id/M/MA/MARCEL/>.
+Archive Network (CPAN). Visit L<http://www.perl.com/CPAN/> to find a CPAN
+site near you, or see
+L<http://search.cpan.org/dist/Text-Pipe-W3CDTF/>.
+
+The development version lives at
+L<http://github.com/hanekomu/Text-Pipe-W3CDTF/>.
+Instead of sending patches, please fork this project using the standard git
+and github infrastructure.
 
 =head1 AUTHOR
 
-Marcel GrE<uuml>nauer, C<< <marcel@cpan.org> >>
+  Marcel Gruenauer <marcel@cpan.org>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright 2007 by Marcel GrE<uuml>nauer
+This software is copyright (c) 2008 by Marcel Gruenauer.
 
-This library is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself.
-
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
 
 =cut
 
